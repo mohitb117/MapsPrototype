dea56ae085156b3a9e4f0188c7a5b033e329464d
Mohit Bhalla <mbhalla@syr.edu>
2013-06-20 15:44:18 -0700

    Initial Commit

---
+++ src/pnnl/gov/mapsPrototype/mapActivity.java
@@ -0,0 +1,31 @@
+package pnnl.gov.mapsPrototype;
+
+import android.app.Activity;
+import android.os.Bundle;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+
+public class mapActivity extends Activity
+    {
+
+        private GoogleMap googleMap;
+        /**
+         * Called when the activity is first created.
+         */
+        @Override
+        public void onCreate(Bundle savedInstanceState)
+        {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.main);
+
+            googleMap = ((MapFragment) getFragmentManager().findFragmentById(R.id.map)).getMap();
+            googleMap.setMapType(GoogleMap.MAP_TYPE_SATELLITE);
+            final LatLng CIU = new LatLng(1.926, 73.400);
+            Marker ciu = googleMap.addMarker(
+                    new MarkerOptions().position(CIU).title("My dreamed island"));
+        }
+    }
